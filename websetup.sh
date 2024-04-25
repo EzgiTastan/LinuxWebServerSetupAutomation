f4c47d4fd485542f6b09a96ba5c8e320e9085480
Ezgi Tastan <kedigunesi@gmail.com>
2024-04-25 12:07:05 +0300

    dismantle.sh is added

--- websetup.sh
+++ websetup.sh
@@ -1,46 +1,53 @@
 #!/bin/bash
 
+#Variable Declaration
+PACKAGE="httpd wget unzip"
+SVC="httpd"
+URL="https://www.tooplate.com/zip-templates/2110_character.zip"
+ART_NAME="2110_character"
+TEMPDIR="/tmp/webfiles"
+
 # Installing Dependencies
 echo "########################################################"
 echo "Installing packages"
 echo "########################################################"
-sudo yum install wget unzip httpd -y > /dev/null
+sudo yum install $PACKAGE -y > /dev/null
 echo
 
 # Start and Enable The Service
 echo "########################################################"
 echo "Start and Enable HTTPD Service"
 echo "########################################################"
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
 echo
 
 #Creating a Temp Directory
 echo "########################################################"
 echo "Starting Artifact Deployment"
 echo "########################################################"
-mkdir -p /tmp/webfiles
-cd /tmp/webfiles
+mkdir -p $TEMPDIR
+cd $TEMPDIR
 echo
 
-wget https://www.tooplate.com/zip-templates/2110_character.zip > /dev/null
-unzip 2110_character.zip > /dev/null
-sudo cp -r 2110_character/* /var/www/html/
+wget $URL > /dev/null
+unzip $ART_NAME.zip > /dev/null
+sudo cp -r $ART_NAME/* /var/www/html/
 echo
 
 #Bounce Service
 echo "########################################################"
 echo "Restarting HTTPD Service"
 echo "########################################################"
-systemctl restart httpd
+systemctl restart $SVC
 echo
 
 #Clean Up
 echo "########################################################"
 echo "Removing Temporary Files"
 echo "########################################################"
-rm -rf /tmp/webfiles
+rm -rf $TEMPDIR
 echo
 
-sudo systemctl status httpd
+sudo systemctl status $SVC
 ls /var/www/html/
